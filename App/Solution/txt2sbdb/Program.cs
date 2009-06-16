6f691f496c130857412ab64637920a8547ab5cc4
Altieres Lopes <altiereslopes@webgoal.com.br>
2009-06-16 02:54:54 +0000

    Solution Tree Specification.

---
+++ App/Solution/txt2sbdb/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace txt2sbdb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- SpokenBible: file.txt to SpokenBible DataBase file converter ---");
+
+            //Code Here!!!
+        }
+    }
+}
